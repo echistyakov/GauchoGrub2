1ce8a1d10b17293bac8112396e3c3e904238e960
Andrew Tran <scorpioandrew777@gmail.com>
2016-03-06 22:02:12 -0800

    Revert "Requery ORM"

--- GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/db/models/Rating.java
+++
@@ -1,5 +0,0 @@
-package edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models;
-
-public class Rating {
-    // TODO: do we need this?
-}
--- GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/db/models/SpecialMeal.java
+++
@@ -1,5 +0,0 @@
-package edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models;
-
-public class SpecialMeal {
-    // Not Implemented: event for holidays and/or finals week, etc.
-}
