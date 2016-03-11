23f534c5378c3d71a70663971da2f0b3175da90b
Andrew Tran <andrew.tran345@gmail.com>
2016-03-11 12:24:22 -0800

    Continued refactoring BootReceiver, removing extraneous alarm

--- GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/utils/BootReceiver.java
+++ GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/utils/BootReceiver.java
@@ -11,6 +11,9 @@ import android.net.NetworkInfo;
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.services.MenuScraperService;
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.services.NotificationService;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Calendar;
 
 public class BootReceiver extends BroadcastReceiver {
@@ -32,12 +35,11 @@ public class BootReceiver extends BroadcastReceiver {
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
             AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
-
             // sets up notification service
             startNotificationService(context, alarmManager);
 
             // sets up data automation service
-
+            startMenuScraperService(context, alarmManager);
         }
     }
 
@@ -45,7 +47,7 @@ public class BootReceiver extends BroadcastReceiver {
         Intent notificationIntent = new Intent(context, NotificationService.class);
         PendingIntent pendingNotificationIntent = PendingIntent.getService(context, 0, notificationIntent, 0);
         Calendar notificationCalendar = Calendar.getInstance();
-        notificationCalendar.set(Calendar.HOUR_OF_DAY, 8);
+        notificationCalendar.set(Calendar.HOUR_OF_DAY, 7);
         notificationCalendar.set(Calendar.MINUTE, 0);
         notificationCalendar.set(Calendar.SECOND, 0);
         alarmManager.setRepeating(AlarmManager.RTC, notificationCalendar.getTimeInMillis(),
@@ -53,19 +55,39 @@ public class BootReceiver extends BroadcastReceiver {
     }
 
     private void startMenuScraperService(Context context, AlarmManager alarmManager) {
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netinfo = cm.getActiveNetworkInfo();
-        Intent timedIntent = new Intent(context, MenuScraperService.class);
-        PendingIntent pendingAutomationIntent = PendingIntent.getService(context, 0, timedIntent, 0);
-        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
-            alarmManager.set(AlarmManager.RTC_WAKEUP, 2 * 60 * 1000, pendingAutomationIntent);
-            context.startService(timedIntent);
+        Intent menuScraperIntent = new Intent(context, MenuScraperService.class);
+        PendingIntent pendingMenuScraperIntent = PendingIntent.getService(context, 0, menuScraperIntent, 0);
+        if (hasInternetAccess(context)) {
+            context.startService(menuScraperIntent);
         }
         Calendar scraperCalendar = Calendar.getInstance();
         scraperCalendar.set(Calendar.HOUR_OF_DAY, 5);
         scraperCalendar.set(Calendar.MINUTE, 0);
         scraperCalendar.set(Calendar.SECOND, 0);
         alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, scraperCalendar.getTimeInMillis(),
-                AlarmManager.INTERVAL_DAY, pendingAutomationIntent);
+                AlarmManager.INTERVAL_DAY, pendingMenuScraperIntent);
+    }
+
+    /* from http://stackoverflow.com/questions/6493517/detect-if-android-device-has-internet-connection */
+    private boolean hasInternetAccess(Context context) {
+        if (hasNetworkAccess(context)) {
+            try {
+                HttpURLConnection urlc = (HttpURLConnection) (new URL("http://clients3.google.com/generate_204").openConnection());
+                urlc.setRequestProperty("User-Agent", "Android");
+                urlc.setRequestProperty("Connection", "close");
+                urlc.setConnectTimeout(1500);
+                urlc.connect();
+                return (urlc.getResponseCode() == 204 && urlc.getContentLength() == 0);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private boolean hasNetworkAccess(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null;
     }
 }
