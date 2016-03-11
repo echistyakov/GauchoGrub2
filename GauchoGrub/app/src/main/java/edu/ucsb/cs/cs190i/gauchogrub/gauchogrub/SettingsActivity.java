c1b03bca5fa99e8980d272962cd4816287f849f4
Andrew Tran <andrew.tran345@gmail.com>
2016-03-10 23:20:15 -0800

    Human-readable text for default filter preference

--- GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/SettingsActivity.java
+++ GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/SettingsActivity.java
@@ -3,25 +3,27 @@ package edu.ucsb.cs.cs190i.gauchogrub.gauchogrub;
 
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
 import android.text.TextUtils;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
+import com.google.common.base.Joiner;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -42,13 +44,11 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
         @Override
         public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
-
             if (preference instanceof ListPreference) {
                 // For list preferences, look up the correct display value in
                 // the preference's 'entries' list.
                 ListPreference listPreference = (ListPreference) preference;
-                int index = listPreference.findIndexOfValue(stringValue);
+                int index = listPreference.findIndexOfValue(value.toString());
 
                 // Set the summary to reflect the new value.
                 preference.setSummary(
@@ -56,10 +56,22 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                                 ? listPreference.getEntries()[index]
                                 : null);
 
+            } else if (preference instanceof MultiSelectListPreference) {
+                MultiSelectListPreference multiSelectListPreference = (MultiSelectListPreference) preference;
+                // We ensure that value objects passed for MultiSelectListPreference preferences
+                // will be of type Set<String>
+                @SuppressWarnings("unchecked")
+                Set<String> setOfStrings = (Set<String>) value;
+                CharSequence[] entriesArray = multiSelectListPreference.getEntries();
+                List<String> selectedEntries = new ArrayList<>();
+                for (String str : setOfStrings) {
+                    selectedEntries.add((String) entriesArray[multiSelectListPreference.findIndexOfValue(str)]);
+                }
+                preference.setSummary(Joiner.on("\n").join(selectedEntries));
             } else {
                 // For all other preferences, set the summary to the value's
                 // simple string representation.
-                preference.setSummary(stringValue);
+                preference.setSummary(value.toString());
             }
             return true;
         }
@@ -89,10 +101,14 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
         // Trigger the listener immediately with the preference's
         // current value.
-        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+        Object value;
+        if (preference instanceof MultiSelectListPreference) {
+            value = sharedPreferences.getStringSet(preference.getKey(), Collections.<String>emptySet());
+        } else {
+            value = sharedPreferences.getString(preference.getKey(), "");
+        }
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference, value);
     }
 
     @Override
@@ -148,7 +164,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("default_dining_common"));
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_key_default_dining_common)));
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_key_default_filter)));
         }
     }
 
