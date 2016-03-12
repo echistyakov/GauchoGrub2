0febccd5c5ed2c122efff561206e0c7fbf94bd52
Andrew Tran <andrew.tran345@gmail.com>
2016-03-12 01:38:54 -0800

    WIP: Attempting to add DB calls to parser

--- GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/services/MenuScraperService.java
+++ GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/services/MenuScraperService.java
@@ -17,7 +17,9 @@ import java.util.Date;
 
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.GGApp;
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.R;
+import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.MenuItem;
 import io.requery.Persistable;
+import io.requery.meta.AttributeBuilder;
 import io.requery.rx.SingleEntityStore;
 import io.requery.sql.EntityDataStore;
 
@@ -28,10 +30,15 @@ import io.requery.sql.EntityDataStore;
  */
 public class MenuScraperService extends IntentService {
     private static final String TAG = "MenuScraperService";
+    private final String BRIGHT_MEAL = getString(R.string.parsable_bright_meal);
+    private final String VEGETARIAN = getString(R.string.parsable_veg);
+    private final String VEGAN = getString(R.string.parsable_vgn);
+    private final String HAS_NUTS = getString(R.string.parsable_has_nuts);
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_SCRAPE_MENU_AT_DATE = "edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.action.SCRAPE_MENU_AT_DATE";
     private static final String EXTRA_PARAM_DATE = "edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.extra.DATE";
     private EntityDataStore<Persistable> mDataStore;
+    private Date mDate;
 
     public MenuScraperService() {
         super("MenuScraperService");
@@ -66,8 +73,9 @@ public class MenuScraperService extends IntentService {
     }
 
     private void scrapeMenus(Date date) {
+        mDate = date;
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        String formattedDate = df.format(date);
+        String formattedDate = df.format(mDate);
         try {
             Document doc = Jsoup.connect(getString(R.string.parsable_menu_url) + "?day=" + formattedDate).get();
             String[] diningCommonIds = getResources().getStringArray(R.array.parsable_dining_commons_ids);
@@ -81,14 +89,16 @@ public class MenuScraperService extends IntentService {
     }
 
     private void parseDiningCommonMenu(Element diningCommonMenu) {
-        String brightMeal = getString(R.string.bright_meal);
+        boolean isVegetarian;
+        boolean isVegan;
+        boolean hasNuts;
         Elements mealPanels = diningCommonMenu.children();
         Elements filteredMealPanels = new Elements();
         // filter panels by "panel-success" class so that we do not
         // scrape warning panels (when meals are not served), and
         // do not include Bright Meal panels
         for (Element panel: mealPanels) {
-            if (panel.hasClass("panel-success") && !panel.getElementsByTag("h5").first().text().equals(brightMeal))
+            if (panel.hasClass("panel-success") && !panel.getElementsByTag("h5").first().text().equals(BRIGHT_MEAL))
                 filteredMealPanels.add(panel);
         }
         for (Element mealPanel : filteredMealPanels) {
@@ -98,8 +108,16 @@ public class MenuScraperService extends IntentService {
                 String menuCategoryStr = foodListByCategory.getElementsByTag("dt").first().text();
                 Log.d(TAG, menuCategoryStr);
                 for (Element menuItem : foodListByCategory.getElementsByTag("dd")) {
-                    String menuItemStr = menuItem.text();
-                    Log.d(TAG, menuItemStr);
+                    String menuItemTitleStr = menuItem.text();
+                    // determine if MenuItem of same MenuCategory and title exists in DB
+
+                    // mDataStore.select(MenuItem.class, )
+
+                    isVegetarian = menuItemTitleStr.contains(VEGETARIAN);
+                    isVegan = menuItemTitleStr.contains(VEGAN);
+                    hasNuts = menuItemTitleStr.contains(HAS_NUTS);
+
+                    Log.d(TAG, menuItemTitleStr);
                 }
             }
         }
