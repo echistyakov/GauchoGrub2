1b1a545cd653106c30c70316886378c95f7db916
Eric Swenson <elswenson@umail.ucsb.edu>
2016-03-13 14:15:56 -0700

    Updated to the new schema changes

--- GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/services/NotificationService.java
+++ GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/services/NotificationService.java
@@ -13,14 +13,15 @@ import org.joda.time.LocalDate;
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.GGApp;
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.MainActivity;
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.R;
-import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.DiningCommonEntity;
-import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.FavoriteEntity;
-import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.MenuEntity;
+import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.BaseMenuItem;
+import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.DiningCommon;
+import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.Favorite;
+import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.Meal;
+import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.Menu;
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.MenuItem;
-import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.RepeatedEventEntity;
+import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.db.models.RepeatedEvent;
 import io.requery.Persistable;
-import io.requery.query.Result;
-import io.requery.rx.SingleEntityStore;
+import io.requery.sql.EntityDataStore;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -31,7 +32,7 @@ public class NotificationService extends IntentService {
 
     public static Logger logger = Logger.getLogger("NotificationService");
 
-    private SingleEntityStore<Persistable> data;
+    private EntityDataStore<Persistable> data;
 
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -50,7 +51,7 @@ public class NotificationService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        List<Favorite> favorites = getAllFavoritesToday();
+        List<FavoriteStruct> favorites = getAllFavoritesToday();
         // Stop if favorites are empty
         if(favorites.size() <= 0) {
             return;
@@ -61,7 +62,7 @@ public class NotificationService extends IntentService {
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0,
                 notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         int i = 0;
-        for(Favorite favorite : favorites) {
+        for(FavoriteStruct favorite : favorites) {
             builder = new NotificationCompat.Builder(getBaseContext())
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setContentText(favorite.menuItem.title + " is at " + favorite.diningCommon + " for " + favorite.mealName);
@@ -76,30 +77,32 @@ public class NotificationService extends IntentService {
      * @param calendar
      * @return
      */
-    private List<Favorite> getFavorites(String diningCommon, Calendar calendar) {
-        ArrayList<Favorite> favorites = new ArrayList<>();
+    private List<FavoriteStruct> getFavorites(String diningCommon, Calendar calendar) {
+        ArrayList<FavoriteStruct> favorites = new ArrayList<>();
+        // Meals
         int dayOfWeek = LocalDate.fromCalendarFields(calendar).getDayOfWeek();
-        // Get DiningCommonEntity from diningCommon string
-        DiningCommonEntity diningCommonEntityResult = data.select(DiningCommonEntity.class).where(DiningCommonEntity.NAME.eq(diningCommon)).get().first();
-        // Get RepeatedEventEntity result for current day of the week and the right dining common
-        List<RepeatedEventEntity> repeatedEvents = data.select(RepeatedEventEntity.class)
-                .where(RepeatedEventEntity.DAY_OF_WEEK.equal(dayOfWeek)
-                        .and(RepeatedEventEntity.DINING_COMMON.eq(diningCommonEntityResult))).get().toList();
-        // Get MenuEntity of the current day
-        List<MenuEntity> menusInDiningCommonToday = data.select(MenuEntity.class)
-                .where(MenuEntity.DATE.eq(LocalDate.fromCalendarFields(calendar))).get().toList();
+        // Get DiningCommon from diningCommon string
+        DiningCommon diningCommonResult = data.select(DiningCommon.class).where(DiningCommon.NAME.eq(diningCommon)).get().first();
+        // Get RepeatedEvent result for current day of the week and the right dining common
+        List<RepeatedEvent> repeatedEvents = data.select(RepeatedEvent.class)
+                .where(RepeatedEvent.DAY_OF_WEEK.equal(dayOfWeek)
+                        .and(RepeatedEvent.DINING_COMMON_ID.eq(diningCommonResult.getId()))).get().toList();
+        // Get Menu of the current day
+        List<Menu> menusInDiningCommonToday = data.select(Menu.class)
+                .where(Menu.DATE.eq(LocalDate.fromCalendarFields(calendar))).get().toList();
         // Get all favorites
-        List<FavoriteEntity> favoriteEntities = data.select(FavoriteEntity.class).get().toList();
-        for(MenuEntity menu : menusInDiningCommonToday) {
-            for(RepeatedEventEntity repeatedEvent : repeatedEvents) {
+        List<Favorite> favoriteEntities = data.select(Favorite.class).get().toList();
+        for(Menu menu : menusInDiningCommonToday) {
+            for(RepeatedEvent repeatedEvent : repeatedEvents) {
                 // If menu is today and corresponds to a repeatedEvent for the correct diningCommon
-                if(menu.getEvent().id == repeatedEvent.getId()) {
+                if(menu.getEventId() == repeatedEvent.getId()) {
                     // Traverse all items in the menu, add favorites to list of favorites
-                    for(FavoriteEntity favoriteEntity : favoriteEntities) {
-                        for(MenuItem menuItem : menu.getMenuItems().toList()) {
+                    for(Favorite favorite : favoriteEntities) {
+                        for(BaseMenuItem menuItem : menu.getMenuItems().toList()) {
                             // If menuItem is favorite, add to list of favorites
-                            if(menuItem.id == favoriteEntity.getId()) {
-                                favorites.add(new Favorite(menuItem, diningCommon, repeatedEvent.getMeal().name));
+                            if(menuItem.id == favorite.getMenuItemId()) {
+                                Meal meal = data.select(Meal.class).where(Meal.ID.eq(repeatedEvent.getMealId())).get().first();
+                                favorites.add(new FavoriteStruct(menuItem, diningCommon, meal.getName()));
                             }
                         }
                     }
@@ -109,13 +112,13 @@ public class NotificationService extends IntentService {
         return favorites;
     }
 
-    private List<Favorite> getFavoritesToday(String diningCommon) {
+    private List<FavoriteStruct> getFavoritesToday(String diningCommon) {
         return getFavorites(diningCommon, Calendar.getInstance());
     }
 
-    private List<Favorite> getAllFavoritesToday() {
+    private List<FavoriteStruct> getAllFavoritesToday() {
         String[] diningCommons = getResources().getStringArray(R.array.dining_commons);
-        ArrayList<Favorite> favorites = new ArrayList<>();
+        ArrayList<FavoriteStruct> favorites = new ArrayList<>();
         for(String diningCommon : diningCommons) {
             favorites.addAll(getFavoritesToday(diningCommon));
         }
@@ -125,13 +128,13 @@ public class NotificationService extends IntentService {
     /**
      * Helper class Favorite for storing relevant data succinctly
      */
-    public class Favorite {
+    public class FavoriteStruct {
 
-        public MenuItem menuItem;
+        public BaseMenuItem menuItem;
         public String mealName;
         public String diningCommon;
 
-        public Favorite(MenuItem menuItem, String diningCommon, String mealName) {
+        public FavoriteStruct(BaseMenuItem menuItem, String diningCommon, String mealName) {
             this.menuItem = menuItem;
             this.mealName = mealName;
             this.diningCommon = diningCommon;
