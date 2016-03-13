7d066480a70e871ae28890f14231328eb293d7ba
Jack Chistyakov <jack.chistyakov@outlook.com>
2016-03-12 23:14:53 -0800

    OneToMany handle added to RepeatedEvent

--- GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/db/models/RepeatedEvent.java
+++ GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/db/models/RepeatedEvent.java
@@ -9,6 +9,8 @@ import io.requery.Entity;
 import io.requery.Generated;
 import io.requery.Key;
 import io.requery.ManyToOne;
+import io.requery.OneToMany;
+import io.requery.query.Result;
 
 /* An event repeated every week on a specific day in a specific DiningCommon during a specific Meal.
  * Has a start and end time. */
@@ -38,5 +40,8 @@ public class RepeatedEvent {
     @Column(nullable = false, index = true, unique = false)
     /* Use day-of-the-week values from Joda DateTimeConstants class (1-indexed) */
     public int dayOfWeek;
+
+    @OneToMany
+    public Result<Menu> menus;
 
 }
