0fccfdffb49fe17f5392adce1cee7d3ae26c72e0
Eric Swenson <elswenson@umail.ucsb.edu>
2016-03-14 20:59:23 -0700

    Implemented Schedule Fragment layouts

--- GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/fragments/ScheduleFragment.java
+++ GauchoGrub/app/src/main/java/edu/ucsb/cs/cs190i/gauchogrub/gauchogrub/fragments/ScheduleFragment.java
@@ -1,15 +1,29 @@
 package edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.fragments;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.adapters.ScheduleRecyclerAdapter;
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.activities.MainActivity;
 import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.R;
+import edu.ucsb.cs.cs190i.gauchogrub.gauchogrub.adapters.FavoriteRecyclerAdapter;
 
 
 /**
@@ -31,6 +45,51 @@ public class ScheduleFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private String diningCommon;
+
+    @Bind(R.id.ScheduleFragment_Monday)
+    RecyclerView recyclerViewMonday;
+
+    private ScheduleRecyclerAdapter mondayRecyclerAdapter;
+    private ExecutorService mondayExecutorService;
+
+    @Bind(R.id.ScheduleFragment_Tuesday)
+    RecyclerView recyclerViewTuesday;
+    
+    private ScheduleRecyclerAdapter tuesdayRecyclerAdapter;
+    private ExecutorService tuesdayExecutorService;
+
+    @Bind(R.id.ScheduleFragment_Wednesday)
+    RecyclerView recyclerViewWednesday;
+    
+    private ScheduleRecyclerAdapter wednesdayRecyclerAdapter;
+    private ExecutorService wednesdayExecutorService;
+
+    @Bind(R.id.ScheduleFragment_Thursday)
+    RecyclerView recyclerViewThursday;
+
+    private ScheduleRecyclerAdapter thursdayRecyclerAdapter;
+    private ExecutorService thursdayExecutorService;
+
+    @Bind(R.id.ScheduleFragment_Friday)
+    RecyclerView recyclerViewFriday;
+
+    private ScheduleRecyclerAdapter fridayRecyclerAdapter;
+    private ExecutorService fridayExecutorService;
+
+    @Bind(R.id.ScheduleFragment_Saturday)
+    RecyclerView recyclerViewSaturday;
+
+    private ScheduleRecyclerAdapter saturdayRecyclerAdapter;
+    private ExecutorService saturdayExecutorService;
+
+    @Bind(R.id.ScheduleFragment_Sunday)
+    RecyclerView recyclerViewSunday;
+
+    private ScheduleRecyclerAdapter sundayRecyclerAdapter;
+    private ExecutorService sundayExecutorService;
+
 
     public ScheduleFragment() {
         // Required empty public constructor
@@ -61,6 +120,10 @@ public class ScheduleFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(getString(R.string.MainActivity_dining_common_shared_prefs), Context.MODE_PRIVATE);
+        diningCommon = sharedPreferences.getString(MainActivity.STATE_CURRENT_DINING_COMMON,
+                sharedPreferences.getString(getString(R.string.pref_key_default_dining_common),
+                        getString(R.string.DLG)));
     }
 
     @Override
@@ -75,7 +138,43 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        ButterKnife.bind(this, view);
+        setScheduleAdapters();
+        return view;
+    }
+
+    public void updateDiningCommon(String diningCommon) {
+        this.diningCommon = diningCommon;
+        setScheduleAdapters();
+    }
+
+    private void setScheduleAdapters() {
+        setScheduleAdapter(recyclerViewMonday, mondayRecyclerAdapter, mondayExecutorService, DateTimeConstants.MONDAY);
+        setScheduleAdapter(recyclerViewTuesday, tuesdayRecyclerAdapter, tuesdayExecutorService, DateTimeConstants.TUESDAY);
+        setScheduleAdapter(recyclerViewWednesday, wednesdayRecyclerAdapter, wednesdayExecutorService, DateTimeConstants.WEDNESDAY);
+        setScheduleAdapter(recyclerViewThursday, thursdayRecyclerAdapter, thursdayExecutorService, DateTimeConstants.THURSDAY);
+        setScheduleAdapter(recyclerViewFriday, fridayRecyclerAdapter, fridayExecutorService, DateTimeConstants.FRIDAY);
+        setScheduleAdapter(recyclerViewSaturday, saturdayRecyclerAdapter, saturdayExecutorService, DateTimeConstants.SATURDAY);
+        setScheduleAdapter(recyclerViewSunday, sundayRecyclerAdapter, sundayExecutorService, DateTimeConstants.SUNDAY);
+    }
+
+    private void setScheduleAdapter(RecyclerView recyclerView, @Nullable ScheduleRecyclerAdapter adapter, @Nullable ExecutorService executorService, int dayOfWeek) {
+        if(adapter != null && executorService != null) {
+            recyclerView.setRecycledViewPool(new RecyclerView.RecycledViewPool());
+            adapter.close();
+            executorService.shutdownNow();
+        }
+        // Create new adapter and executor
+        adapter = new ScheduleRecyclerAdapter(diningCommon, dayOfWeek, getContext());
+        executorService = Executors.newSingleThreadExecutor();
+        // Set executor and adapter
+        adapter.setExecutor(executorService);
+        recyclerView.setAdapter(adapter);
+        // Set layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // Start async query
+        adapter.queryAsync();
     }
 
     // TODO: Rename method, update argument and hook method into UI event
